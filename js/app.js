8c9bd7f88eb91f1d5d092b4a89c7d650cc307c9d
Maryucha_Mariani <https://Maryucha_Mariani@bitbucket.org/Maryucha_Mariani/appsensores.git>
2021-08-12 17:58:42 -0300

    fazendo a requisição pra api

--- js/app.js
+++ js/app.js
@@ -1,8 +1,23 @@
 'use strict';
 
-const pesquisarCep = () =>{
+const preencherFormulario = (endereco) => {
+    document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('numero').value = endereco.numero;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}
+
+ const pesquisarCep = async() =>{
     const cep = document.getElementById('cep').value;
-    alert(cep);
+    const url= `http://viacep.com.br/ws/${cep}/json/`;
+
+    const dados = await fetch(url);
+    const endereco = await dados.json();
+    preencherFormulario(endereco);
+
+    //ele faz tudo é assincrono e tras uma pomisse ai eu pego apenas a responsse e trato ela pelo metodo json
+   // fetch(url).then(response => response.json()).then(console.log);
 }
 
 document.getElementById('cep')
